e3e5401f570401a10fb14876c4ccc023c514a46a
zhaojinzhi <zhaojinzhi@cienet.com.cn>
2016-02-19 18:29:56 +0800

    add 2 file

---
+++ file.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+
+# 打开一个文件
+fo = open("foo.txt", "wb")
+print "文件名: ", fo.name
+print "是否已关闭 : ", fo.closed
+print "访问模式 : ", fo.mode
+print "末尾是否强制加空格 : ", fo.softspace
---
+++ sample5.py
@@ -0,0 +1,7 @@
+#coding:utf-8
+list = []
+for i in range(3):
+    x = int(raw_input('integer:\n'))
+    list.append(x)
+list.sort()
+print list
